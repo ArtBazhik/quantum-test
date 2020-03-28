62c34bbe0995d5ae6fc8d9b0fa7c1d02dabf8c4e
ArtemKorovkin <art888kor@gmail.com>
2020-03-28 18:48:59 +1000

    Started work on section Slider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.scss';
 import Header from './Components/Header'
+import SlickSlider from './Components/SlickSlider'
 
 
 const App = () => {
   return (
     <div className="wrapper">
       <Header/>
+      <SlickSlider/>
     </div>
-
   );
 }
 
